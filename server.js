b2765ed3f28d8136a133b2a034ef0bfaa3539a52
Unknown <sztuczynski.grzegorz@gmail.com>
2018-10-17 18:00:00 +0100

    simple user schema

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-let express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+// DataBase Connection
+mongoose.connect('mongodb://127.0.0.1/login', {useMongoClient: true}, ()=>{
+
+    console.log('connected');
+});
 
 // App declaration
 let app = express();
@@ -7,9 +16,9 @@ let app = express();
 
 
 // MIDDLEWARE
-
 app.use(express.static('public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 
